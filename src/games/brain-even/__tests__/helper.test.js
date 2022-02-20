263425694b57664edf2eb3f9c297bf4ed1b5d5e9
Ilya Lokalin <ilyalokalin@gmail.com>
2022-02-21 03:28:30 +0500

    test: remove rules case

--- src/games/brain-even/__tests__/helper.test.js
+++ src/games/brain-even/__tests__/helper.test.js
@@ -7,20 +7,14 @@ import {
 import {
   createQuestion,
   initAnswersState,
-  showRules,
 } from '../helper.js';
 
 describe('brain-even game helper', () => {
-  it('initStore shold be increment and return 2', () => {
+  it('initStore should be increment and return 2', () => {
     const { incrementCounter, getCounter } = initAnswersState();
     incrementCounter();
     incrementCounter();
     expect(getCounter()).toBe(2);
-  });
-  it('showRules', () => {
-    console.log = jest.fn();
-    showRules();
-    expect(console.log).toHaveBeenCalledWith('Answer "yes" if the number is even, otherwise answer "no".');
   });
   it('createQuestion', () => {
     Math.random = jest.fn(() => 0.5);
